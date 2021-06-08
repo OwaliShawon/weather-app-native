ec2732aab9227994388fd1c4569f7ff6626021e9
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2021-06-09 02:35:21 +0600

    location latitude and lognitude from expo api

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [location, setLocation] = useState(null);
+  useEffect(() => {
+    load();
+  }, [])
+
+  async function load() {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMessage('Access to location is needed');
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync();
+      // alert(`Location: ${location}`)
+      const { latitude, longitude } = location.coords;
+      alert(`Latitude: ${latitude} \n Longitude: ${longitude}`)
+    } catch (error) {
+
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Welcome to Weather App</Text>
       <StatusBar style="auto" />
-      <View style={styles.find}>
-        <Text>Find Weather</Text>
-      </View>
-
-
     </View>
 
   );
