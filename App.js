f103726f70dc0f00a2ddd0baff85a6c0bdf8175f
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2021-06-09 14:53:18 +0600

    data call from open weather api

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const api_key = '58fa72b424e6c8a30df9f61094b8f84f';
   const [errorMessage, setErrorMessage] = useState(null);
   const [location, setLocation] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitsSystem, seUnitsSystem] = useState('metric');
+
   useEffect(() => {
     load();
   }, [])
@@ -21,18 +25,43 @@ export default function App() {
       // alert(`Location: ${location}`)
       const { latitude, longitude } = location.coords;
       alert(`Latitude: ${latitude} \n Longitude: ${longitude}`)
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${api_key}`;
+      // const response = await fetch(weatherUrl)
+      // const result = await response.json();
+      // if (response.ok) {
+      //   setCurrentWeather(result)
+      // } else {
+      //   setErrorMessage(result.message);
+      // }
+
+      fetch(weatherUrl)
+        .then(response => response.json())
+        .then(data => setCurrentWeather(data))
+      // .then(error => setErrorMessage(error))
+
     } catch (error) {
 
     }
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Weather App</Text>
-      <StatusBar style="auto" />
-    </View>
-
-  );
+  if (currentWeather) {
+    const { main: { temp } } = currentWeather
+    console.log(temp)
+    return (
+      <View style={styles.container}>
+        <Text>CurrentWeather: {temp} Degree Celcius</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
