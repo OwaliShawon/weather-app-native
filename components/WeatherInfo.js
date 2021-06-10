2c8f6297fa6ab3d23b8e88f6c8021d9e8f905ec9
Mainul Hasan <76752500+SMMainulHasan@users.noreply.github.com>
2021-06-10 14:39:35 +0600

    weather info updated

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -5,5 +5,47 @@ import {colors} from '../utils/index'
 const {PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR} = colors;
 
 export default function WeatherInfo({currentWeather}) {
-    
+    const {
+        main: {temp},
+        weather: [details],
+        name,
+    } = currentWeather
+
+    const {icon, main, description} = details
+    const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
+
+
+    return (
+        <View style={styles.WeatherInfo}>
+            <Text>{name}</Text>
+            <Image style={styles.weatherIcon} source={iconUrl} />
+            <Text style={styles.textPrimary}>{temp}°</Text>
+            <Text style={styles.weatherDescription}>{description}</Text>
+            <Text style={styles.textSecondary}>{main}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    WeatherInfo: {
+        alignItems: 'center'
+    },
+    weatherDescription: {
+        textTransform: 'capitalize'
+    },
+    weatherIcon: {
+        width: 100,
+        height: 100,
+    },
+    textPrimary: {
+        fontSize: 40,
+        fontWeight: 600,
+        color: PRIMARY_COLOR,
+    },
+    textSecondary: {
+        fontSize: 20,
+        fontWeight: 500,
+        color: SECONDARY_COLOR,
+        marginTop: 10,
+    }
 })
